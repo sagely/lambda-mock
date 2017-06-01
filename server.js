8b846d002dd567ce4f6db7c0cde1778b141bf3f2
Justin Zaun <jzaun@gosagely.com>
2017-06-01 11:12:28 -1000

    Add logging.

--- server.js
+++ server.js
@@ -12,14 +12,18 @@ app.use(require('body-parser').json({limit: '5mb', type: function () { return tr
 
 app.use(function (req, res) {
   var fn = decodeURIComponent(req.url.split('/')[3]).split('.');
+  var name = fn[0].replace('/build/package', '');
   var lambda = require('../' + fn[0]);
   var handler = lambda[fn[1] || 'handler'];
 
+  console.log(name + ': [start]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
   handler(req.body, {
     succeed: function (result) {
+      console.log(name + ': [succeed]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
       res.status(200).json(result);
     },
     fail: function (result) {
+      console.log(name + ': [fail]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
       res.append('X-Amz-Function-Error', 'Handled');
       res.status(200).json({ errorMessage: result });
     }
