3dab4b9ecce476d1e57ce66620a9c0185b31c968
Justin Zaun <jzaun@gosagely.com>
2017-06-13 14:58:23 -1000

    Catch sending duplicate responses.

--- server.js
+++ server.js
@@ -13,19 +13,40 @@ app.use(require('body-parser').json({limit: '5mb', type: function () { return tr
 app.use(function (req, res) {
   var fn = decodeURIComponent(req.url.split('/')[3]).split('.');
   var name = fn[0].replace('/build/package', '');
-  var lambda = require('../' + fn[0]);
+  var lambda;
+  try {
+    lambda = require('../' + fn[0]);
+  } catch (err) {
+    console.log(name + ': [error]   ' + (req.url) + ' ' + err.message);
+  }
+
+  if (!lambda) {
+    return res.status(500).json({ errorMessage: 'Could not load lambda' });
+  }
+
   var handler = lambda[fn[1] || 'handler'];
 
   console.log(name + ': [start]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
+  var sent = false;
   handler(req.body, {
     succeed: function (result) {
-      console.log(name + ': [succeed]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
-      res.status(200).json(result);
+      if (!sent) {
+        console.log(name + ': [succeed]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
+        res.status(200).json(result);
+      } else {
+        console.log(name + ': [dup]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
+      }
+      sent = true;
     },
     fail: function (result) {
-      console.log(name + ': [fail]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
-      res.append('X-Amz-Function-Error', 'Handled');
-      res.status(200).json({ errorMessage: result });
+      if (!sent) {
+        console.log(name + ': [fail]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
+        res.append('X-Amz-Function-Error', 'Handled');
+        res.status(200).json({ errorMessage: result });
+      } else {
+        console.log(name + ': [dup]   ' + (req.body.requestType || '') + ' ' + (req.body.type || 'Unknown mime'));
+      }
+      sent = true;
     }
   });
 });
